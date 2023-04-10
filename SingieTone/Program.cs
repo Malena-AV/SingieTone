5172790ffe6f27d9f0bd38d0f78b583bc878a12c
Malena-AV <m.alena07082003@gmail.com>
2023-04-10 20:10:42 +0300

    Добавьте файлы проекта.

---
+++ SingieTone/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingieTone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTone.GetExample();
+        }
+    }
+}
---
+++ SingieTone/SingleTone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingieTone
+{
+    // паттерн гарантирует наличие только одного экземпляра
+    class SingleTone
+    {
+        private static SingleTone single;
+
+        // приватный конструктор - закрываем доступ для создания экземпляров
+        private SingleTone() {}
+
+        public static SingleTone GetExample()
+        {
+            if (single == null)
+            {
+                single = new SingleTone();
+            }
+            return single;
+        }
+    }
+}
